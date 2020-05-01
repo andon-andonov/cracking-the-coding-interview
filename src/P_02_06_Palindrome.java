e703102b3c2928c5893ed7623f6e08d52471a205
Andon Andonov <andon.andonov@gmail.com>
2020-05-02 00:51:32 +0300

    solve 2.6 with recursive algorith

--- src/P_02_06_Palindrome.java
+++ src/P_02_06_Palindrome.java
@@ -7,6 +7,7 @@ public class P_02_06_Palindrome {
 	public static void main(String[] args) {
 		ListNode list = ListNode.fromArray(new int[] { 1, 2, 3, 2, 1 });
 		System.out.println(isPalindrome(list));
+		System.out.println(isPalindromeRecursive(list));
 	}
 	
 	public static boolean isPalindrome(ListNode head) {
@@ -28,6 +29,35 @@ public class P_02_06_Palindrome {
 			slow = slow.next;
 		}
 		return true;
+	}
+	
+	public static boolean isPalindromeRecursive(ListNode head) {
+		Result result = isPalindromeRecursive(head, head);
+		return result.isPalindrome;
+	}
+	
+	private static Result isPalindromeRecursive(ListNode slow, ListNode fast) {
+		if (fast == null) {
+			return new Result(true, slow);
+		}
+		if (fast.next == null) {
+			return new Result(true, slow.next);
+		}
+		
+		Result result = isPalindromeRecursive(slow.next, fast.next.next);
+		boolean isPalindrome = result.isPalindrome && slow.data == result.compareNode.data;
+		return new Result(isPalindrome, result.compareNode.next);
+		
+	}
+	
+	private static class Result {
+		public boolean isPalindrome;
+		public ListNode compareNode;
+		
+		public Result(boolean isPalindrome, ListNode compareNode) {
+			this.isPalindrome = isPalindrome;
+			this.compareNode = compareNode;
+		}
 	}
 
 }
